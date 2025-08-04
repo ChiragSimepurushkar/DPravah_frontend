e0c1e84c796654b6e3dc5751724841bae978925c
Chirag Nikant Simepurushkar <simepurushkarchirag84@gmail.com>
2025-08-04 23:08:23 +0530

    fix: Remove trailing slash from dashboard API baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'https://d-pravah-backend.vercel.app/', // Your backend URL
+  baseURL: 'https://d-pravah-backend.vercel.app', // Your backend URL
   withCredentials: true, // This is crucial for sending cookies
 });
 
