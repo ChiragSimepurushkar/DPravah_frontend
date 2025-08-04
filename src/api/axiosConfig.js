3f102dda5d65f34ececae9d7c70ed547085fcaf4
Chirag Nikant Simepurushkar <simepurushkarchirag84@gmail.com>
2025-08-04 22:44:26 +0530

    fix: Update API URL to live backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:3002', // Your backend URL
+  baseURL: 'https://d-pravah-backend.vercel.app/', // Your backend URL
   withCredentials: true, // This is crucial for sending cookies
 });
 
